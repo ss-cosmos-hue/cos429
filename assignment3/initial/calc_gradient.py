116085341ea56c16b76d967c202501db26f30bca
ss-cosmos-hue <122190026+ss-cosmos-hue@users.noreply.github.com>
2023-03-26 10:53:59 -0400

    implemented  calc_grad (without checking)

--- assignment3/initial/calc_gradient.py
+++ assignment3/initial/calc_gradient.py
@@ -15,17 +15,23 @@ def calc_gradient(model, input, layer_acts, dv_output):
         grads:  A list of gradients of each layer in model["layers"]
     '''
     num_layers = len(model["layers"])
-    grads = [None,] * num_layers
+    grads = [None,] * num_layers#in order
 
     # TODO: Determine the gradient at each layer.
     #       Remember that back-propagation traverses 
     #       the model in the reverse order.
-
-    # WIP
-    for l in range(num_layers)[::-1]:
-        layer = model['layers'][l]
-        print(layer['fwd_fn'](input, layer['params'], layer['hyper_params'], False))
-        _, dv_input, grads[l] = layer['fwd_fn'](input, layer['params'], layer['hyper_params'], True, dv_output=dv_output)
-        dv_output = dv_input
+    backprop = True
+    assert (num_layers>=2),num_layers
+    
+    layer = model['layers'][-1]
+    _,dv_input,grads[-1] = layer['fwd_fn'](layer_acts[-1],layer['params'],layer['hyperparams'],backprop,dv_output=dv_output)
+    
+    for i in np.arange(1,num_layers-1)[::-1]:
+        layer = model['layers'][i]
+        _,dv_input,grads[i] = layer['fwd_fn'](layer_acts[i-1],layer['params'],layer['hyperparams'],backprop,dv_output=dv_input)
+    
+    layer = model['layers'][0]
+    _,dv_input,grads[0] = layer['fwd_fn'](input,layer['params'],layer['hyperparams'],backprop,dv_input)
+    
 
     return grads
