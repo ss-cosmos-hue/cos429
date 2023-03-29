115f767dd5b2dcac2e58c14e6f1e31d4b6dd84fc
ss-cosmos-hue <122190026+ss-cosmos-hue@users.noreply.github.com>
2023-03-29 14:23:17 -0400

    stop applying weight decay to bias

--- assignment3/initial/update_weights.py
+++ assignment3/initial/update_weights.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def update_weights(model, grads, hyper_params):
+def update_weights(model, grads, hyper_params,ave_grads = None):
     '''
     Update the weights of each layer in your model based on the calculated gradients
     Args:
@@ -15,13 +15,15 @@ def update_weights(model, grads, hyper_params):
     num_layers = len(grads)
     a = hyper_params["learning_rate"]
     lmd = hyper_params["weight_decay"]
+    #not sure how to use it https://towardsdatascience.com/this-thing-called-weight-decay-a7cd4bcfccab
     updated_model = model
 
     # TODO: Update the weights of each layer in your model based on the calculated gradients
-    for l in range(num_layers):
-        if updated_model['layers'][l]['params']['W'] is not None:
-            updated_model['layers'][l]['params']['W'] -= a * (grads[l]['W'] - lmd * updated_model['layers'][l]['params']['W'])
-        if updated_model['layers'][l]['params']['b'] is not None:
-            updated_model['layers'][l]['params']['b'] = a * (grads[l]['b'] - lmd * updated_model['layers'][l]['params']['b'])
+    
+    for i in range(num_layers):
+        layer = model['layers'][i]
+        if layer['type']=='linear' or layer['type']=='conv':
+            layer['params']['W']-=a*(grads[i]['W']+lmd*layer['params']['W'])#though I'm not sure
+            layer['params']['b']-=a*grads[i]['b']
 
     return updated_model
