d4f3c40f8b811ccfe3fd3d2083a49cb1f115fd6c
ss-cosmos-hue <122190026+ss-cosmos-hue@users.noreply.github.com>
2023-04-25 15:47:28 -0400

    working files from adroit

--- finalproject/configurations.py
+++ finalproject/configurations.py
@@ -1,7 +1,9 @@
+
+
 class configurations:
     classes = ["cardboard","glass","metal","paper","plastic","trash"]
     class_sizes = {"cardboard":403,"glass":501,"metal":410,"paper":594,"plastic":482,"trash":137}
     class_labels =  {"cardboard":0,"glass":1,"metal":2,"paper":3,"plastic":4,"trash":5}
-    class_nums = 6
+    num_classes = 6
     height = 512
     width = 384
